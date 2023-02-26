ef05d6d5e41a51b39a2e0d084da2a7bdcd905ef5
masilvax <jakub.bartek@gmail.com>
2023-02-26 16:51:24 +0100

    piggy mouse interaction

--- src/app.ts
+++ src/app.ts
@@ -364,6 +364,24 @@ contactCards[1].addEventListener('mouseenter', () => {
   }
 })
 
+/* Mouse interaction with piggy-contact */
+const piggyContact = document.querySelector('.mouse-track') 
+document.addEventListener("mousemove", e => rotatePiggyContact(e,piggyContact as HTMLElement))
+
+const rotatePiggyContact = (event: MouseEvent, element: HTMLElement) => {
+  const x = event.clientX
+  const y = event.clientY
+
+  const middleX = window.innerWidth/2
+  const middleY = window.innerHeight/2
+
+  const offsetX = ((x - middleX)/middleX) * 45
+  const offsetY = ((y - middleY)/middleY) * 45
+
+  element.style.setProperty("--rotateX", -1 * offsetY + "deg")
+  element.style.setProperty("--rotateY", offsetX + "deg")
+}
+
 /* Experience section - skills and projects */
 let options = {
   root: rootElement,
